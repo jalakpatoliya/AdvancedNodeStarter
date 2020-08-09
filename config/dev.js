37dfd8bd46dc5bb090d39613c135a6fb2d947c22
jalak <jalakpatoliya777@gmail.com>
2020-08-09 11:03:29 +0530

    basic setup

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,7 @@
 module.exports = {
   googleClientID:
-    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
-  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
-  cookieKey: '123123123',
+    `${process.env.GOOGLE_CLIENT_ID}`,
+  googleClientSecret: `${process.env.GOOGLE_CLIENT_SECRET}`,
+  mongoURI: `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@ds159546.mlab.com:59546/blogpost`,
+  cookieKey: "123123123",
 };
